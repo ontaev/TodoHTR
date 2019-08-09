7e7a8ff1cd4179b90b7d83ff917755a871dd9ad0
Badma Ontaev <ontaev@gmail.com>
2019-08-09 22:07:28 +0300

    changes in main

--- main.py
+++ main.py
@@ -7,19 +7,85 @@ from SamplePreprocessor import preprocess
 
 class Params:
     char_list = ""
-    input_image = "input/test.jpg"
+    input_image = "data/test.jpg"
     dataset = "DATASET/"
 
 def train(model, loader):
     """ train NN """
-    print("Train model\n")
+
+    epoch = 0 # number of training epochs since start
+    bestCharErrorRate = float('inf') # best valdiation character error rate
+    noImprovementSince = 0 # number of epochs no improvement of character error rate occured
+    earlyStopping = 5 # stop training after this number of epochs without improvement
+    while True:
+        epoch += 1
+        print('Epoch:', epoch)
+
+        # train
+        print('Train NN')
+        loader.trainSet()
+        while loader.hasNext():
+            iterInfo = loader.getIteratorInfo()
+            batch = loader.getNext()
+            loss = model.trainBatch(batch)
+            print('Batch:', iterInfo[0],'/', iterInfo[1], 'Loss:', loss)
+
+        # validate
+        charErrorRate = validate(model, loader)
+        
+        # if best validation accuracy so far, save model parameters
+        if charErrorRate < bestCharErrorRate:
+            print('Character error rate improved, save model')
+            bestCharErrorRate = charErrorRate
+            noImprovementSince = 0
+            model.save()
+            open(FilePaths.fnAccuracy, 'w').write('Validation character error rate of saved model: %f%%' % (charErrorRate*100.0))
+        else:
+            print('Character error rate not improved')
+            noImprovementSince += 1
+
+        # stop training if no more improvement in the last x epochs
+        if noImprovementSince >= earlyStopping:
+            print('No more improvement since %d epochs. Training stopped.' % earlyStopping)
+            break
 
 def validate(model, loader):
     """ validate NN """
-    print("Validate model\n")
+    print('Validate NN')
+    loader.validationSet()
+    numCharErr = 0
+    numCharTotal = 0
+    numWordOK = 0
+    numWordTotal = 0
+    while loader.hasNext():
+        iterInfo = loader.getIteratorInfo()
+        print('Batch:', iterInfo[0],'/', iterInfo[1])
+        batch = loader.getNext()
+        (recognized, _) = model.inferBatch(batch)
+        
+        print('Ground truth -> Recognized')    
+        for i in range(len(recognized)):
+            numWordOK += 1 if batch.gtTexts[i] == recognized[i] else 0
+            numWordTotal += 1
+            dist = editdistance.eval(recognized[i], batch.gtTexts[i])
+            numCharErr += dist
+            numCharTotal += len(batch.gtTexts[i])
+            print('[OK]' if dist==0 else '[ERR:%d]' % dist,'"' + batch.gtTexts[i] + '"', '->', '"' + recognized[i] + '"')
+    
+    # print validation result
+    charErrorRate = numCharErr / numCharTotal
+    wordAccuracy = numWordOK / numWordTotal
+    print('Character error rate: %f%%. Word accuracy: %f%%.' % (charErrorRate*100.0, wordAccuracy*100.0))
+    return charErrorRate
 
-def recognize(model, input_image):
+def infer(model, input_image):
     """ recognize input image """
+
+    img = preprocess(cv2.imread(fnImg, cv2.IMREAD_GRAYSCALE), Model.imgSize)
+    batch = Batch(None, [img])
+    (recognized, probability) = model.inferBatch(batch, True)
+    print('Recognized:', '"' + recognized[0] + '"')
+    print('Probability:', probability[0])
 
 def main():
     """ main function """
@@ -34,6 +100,9 @@ def main():
 
         loader = DataLoader(Params.dataset, Model.batch_size, Model.image_size, Model.max_text_len)
 
+        # save characters of model for inference mode
+        open(FilePaths.fnCharList, 'w').write(str().join(loader.charList))
+
         if args.train:
 
             print("Training NN!\n")
@@ -47,7 +116,8 @@ def main():
 
     else:
         print("Recognizing image from "+ Params.input_image + " file!\n")
-        pass
+        model = Model(open(FilePaths.fnCharList).read(), decoderType, mustRestore=True, dump=args.dump)
+        infer(model, FilePaths.fnInfer)
 
 if __name__ == '__main__':
     main()
